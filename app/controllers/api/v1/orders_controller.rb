79556a91ce8cb191d778392cd78b45b5620fe2cb
Julian Kohlman <juliankohlman@gmail.com>
2019-08-20 20:45:12 -0400

    Debugging NameError from orders_controller ship method

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,41 +1,64 @@
 module Api
-  module V1
-    class OrdersController < ApplicationController
-      def index
-        if params[:customer_id].present?
-          @orders = Order.where(customer_id: params[:customer_id])
-        else
-          @orders = Order.all
-        end
+	module V1
+		class OrdersController < ApplicationController
+			def index
+				if params[:customer_id].present?
+					@orders = Order.where(customer_id: params[:customer_id])
+				else
+					@orders = Order.all
+				end
 
-        render json: @orders
-      end
+				render json: @orders
+			end
 
-      def show
-        @order = Order.find(params[:id])
+			def show
+				@order = Order.find(params[:id])
 
-        render json: @order
-      end
+				render json: @order
+			end
 
-      def create
-        @order = Order.new(customer_id: params[:customer_id], status: "pending")
+			def create
+				@order = Order.new(customer_id: params[:customer_id], status: 'pending')
 
-        if @order.save
-          render json: @order, status: :created, location: api_v1_order_url(@order)
-        else
-          render json: @order.errors, status: :unprocessable_entity
-        end
-      end
+				if @order.save
+					render json: @order,
+					       status: :created,
+					       location: api_v1_order_url(@order)
+				else
+					render json: @order.errors, status: :unprocessable_entity
+				end
+			end
 
-      def ship
-        @order = Order.find(params[:id])
+			def ship
+				@order = Order.find(params[:id])
 
-        if @order.update(status: "shipped")
-          render json: @order, status: :ok, location: api_v1_order_url(@order)
-        else
-          render json: @order.errors, status: :unprocessable_entity
-        end
-      end
-    end
-  end
+				if @order.ship
+					render json: @order, status: :ok, location: api_v1_order_url(@order)
+				else
+					render json: { message: 'There was a problem shipping your order' },
+					       status: :unprocessable_entity
+
+					# product_ids =
+					# 	OrderProduct.where(order_id: params[:id]).pluck(:product_id)
+					# @products = Product.find(product_ids)
+
+					# shippable = @order.status != 'shipped' && @products.count >= 1
+
+					# if @order.update(status: 'shipped')
+					# 	render json: @order, status: :ok, location: api_v1_order_url(@order)
+					# else
+					# 	render json: @order.errors, status: :unprocessable_entity
+					# end
+
+					# if shippable && @order.update(status: 'shipped')
+					# 	# @order.update(status: 'shipped')
+					# 	render json: @order, status: :ok, location: api_v1_order_url(@order)
+					# else
+					# 	render json: { message: 'There was a problem shipping your order' },
+					# 	       status: :unprocessable_entity
+					# render json: @order.errors, status: :unprocessable_entity
+				end
+			end
+		end
+	end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,17 @@
 class Order < ApplicationRecord
+	def products
+		order = Order.find(params[:id])
+		product_ids = OrderProduct.where(order_id: params[:id]).pluck(:product_id)
+		products = Product.find(product_ids)
+		products
+	end
+
+	def shippable?
+		order = Order.find(params[:id])
+		order.status != 'shipped' && order.products.count >= 1
+	end
+
+	def ship
+		shippable? && order.update(status: 'shipped') ? true : false
+	end
 end
